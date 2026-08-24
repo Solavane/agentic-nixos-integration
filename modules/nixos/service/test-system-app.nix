88e0fb6125410d9612d1e615f26d5ba27d965fee
Solavane <solavane@proton.me>
2026-08-24 23:33:06 +0200

    removed stray configs agent left behind

--- modules/nixos/service/test-system-app.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-import ../../../lib/mkSystemApp.nix {
-  inherit config lib pkgs;
-  name = "$name";
-  packages = [ pkgs.$name ];
-}
--- modules/nixos/service/test-system-service.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-import ../../../lib/mkSystemApp.nix {
-  inherit config lib pkgs;
-  name = "test-system-service";
-  packages = [ pkgs.test-system-service ];
-}
